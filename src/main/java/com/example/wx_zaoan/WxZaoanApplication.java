45652707536b1a08431ff3aa7645e15979c3d233
YangChenxi <1253324157@qq.com>
2022-09-22 21:31:25 +0800

    Init

---
+++ src/main/java/com/example/wx_zaoan/WxZaoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.wx_zaoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxZaoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxZaoanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wx_zaoan/WxZaoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wx_zaoan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxZaoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
